c156c0626b4df9e13ab805c5d1232798314fee70
Chris Howe-Jones <chris.howejones@gmail.com>
2013-11-11 12:23:54 +0000

    Added database functionality to persist the people.

--- src/rest_api/model/people.clj
+++ src/rest_api/model/people.clj
@@ -1,16 +1,37 @@
 ;; Responsible for generating the data model for people
 (ns rest-api.model.people
-  (:use [clojure.data.json :only [read-str write-str]]))
+  (:use [clojure.data.json :only [read-str write-str]]
+        hyperion.api))
 
-(defn create-person [m]
-  "Takes a map of name-value pairs and creates a new person."
-  (if-let [id (:id m)]
-    m)); Need to refactor to store person in database
+;; Set up database
+;;
 
-(defn json-to-person [s]
-  "Convert json string to person"
-  (:person (read-str s :key-fn keyword)))
+(binding [*ds* (new-datastore :implementation :mysql :connection-url "jdbc:mysql://localhost:3306/restapi?user=root" :database "restapi")]
 
-(defn person-to-json [p]
-  "Convert person to json string."
-  (let [pers (assoc-in {} [:person] p)] (write-str pers :key-fn name)))
+  ;; Persistence for person
+
+  (defn create-person [m]
+    "Takes a map of name-value pairs and creates a new person."
+    (if-let [id (:id m)]
+      (save {:kind :person} m)))
+
+  ;; Finders for persom
+
+  (defn find-person [id]
+    "Find a person using the provided unique id. Returns a seq of matching persons."
+    (find-by-kind :person :filters [:= :id id])
+    )
+
+  (defn find-person-by-name [name]
+    "Find a person using the provided name. Returns a seq of matching persons."
+    (find-by-kind :person :filters [:= :name name]))
+
+  ;; Utilities for JSON
+  (defn json-to-person [s]
+    "Convert json string to person"
+    (:person (read-str s :key-fn keyword)))
+
+  (defn person-to-json [p]
+    "Convert person to json string."
+    (let [pers (assoc-in {} [:person] p)] (write-str pers :key-fn name)))
+)
--- test/rest_api/test/model/people.clj
+++ test/rest_api/test/model/people.clj
@@ -1,15 +1,43 @@
 ;; Midje tests for people model.
 (ns rest-api.test.model.people
   (:use midje.sweet
-        rest-api.model.people)
-  )
-
-;; Tests for creating people
-(fact "Test creating a person using id as key"
-      (create-person {:id 999 :name "Bill"}) => {:id 999 :name "Bill" })
+        rest-api.model.people
+        hyperion.api))
 
 (fact "Test converting json representation of person to a map"
       (json-to-person "{\"person\": {\"id\": 123 \"name\": \"Fred\"}}") => {:id 123 :name "Fred"})
 
 (fact "Test converting person (map) to a json string."
       (person-to-json {:id 123 :name "Fred"}) => "{\"person\":{\"name\":\"Fred\",\"id\":123}}")
+
+;; Set up memory ds
+(def mem-ds (new-datastore :implementation :memory))
+
+(binding [*ds* mem-ds]
+
+  ;; Tests for fetching people
+  (do (save {:kind :person :id 111 :name "Test"})
+      (fact "Test to fetch a person using their id."
+            (let [found (find-person 111)]
+              (count found) => 1 ; check only one row found
+              (:id (first found)) => 111
+              (:name (first found)) => "Test"))
+      (fact "Test to fetch a person using their name."
+            (let [found (find-person-by-name "Test")]
+              (count found) => 1
+              (:name (first found)) => "Test"
+              (:id (first found)) => 111)))
+
+  ;; add additional tests here
+;; Tests for creating people
+(fact "Test creating a person using id as key"
+      (dissoc (create-person {:id 999 :name "Bill"}) :key) => {:id 999 :name "Bill" :kind (name :person)})
+
+
+(fact "Test to create a person."
+      (let [created (create-person {:id 333 :name "number 333"})]
+        (:id created) => 333
+        (:name created) => "number 333"
+        (let [fetched (find-by-key (:key created))]
+          fetched => created)))
+  )
